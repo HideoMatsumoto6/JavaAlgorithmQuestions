00fa39fc3377d99f37bfe545fa8346374fb1f495
Yuhan Zhang <Yuhan.Zhang@sandstone.com.au>
2024-08-29 16:59:54 +1000

    generateSubsets

---
+++ src/GenerateSets.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class GenerateSets {
+    static Set<Set<Integer>> generateSubsetsOfLengthK(int[] nums, int k) {
+        Set<Set<Integer>> result = new HashSet<>();
+        generateSubsets(nums, k, 0, new HashSet<>(), result);
+        return result;
+   }
+
+
+   public static void generateSubsets(int[] nums, int k, int index, Set<Integer> current, Set<Set<Integer>> result) {
+        if(current.size()==k) {
+            result.add(new HashSet<>(current));
+            System.out.println(Arrays.toString(current.toArray()));
+            System.out.println(Arrays.toString(result.toArray()));
+            return;
+        }
+        for(int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(nums, k, i + 1, current, result);
+            current.remove(nums[i]);
+        }
+
+   }
+    public static void main(String[] args) {
+        Set<Integer> hashSet1 = new HashSet<>();
+        hashSet1.add(1);
+        hashSet1.add(2);
+
+
+        Set<Integer> hashSet2 = new HashSet<>();
+        hashSet2.add(1);
+        hashSet2.add(2);
+
+        System.out.println(hashSet1 == hashSet2);
+
+        HashMap<Integer, String> hashM1 = new HashMap<>();
+        hashM1.put(1, "James");
+        hashM1.put(2, "Dental");
+
+
+        HashMap<Integer, String> hashM2 = new HashMap<>();
+        hashM2.put(1, "James");
+        hashM2.put(2, "Dental");
+        System.out.println(hashM1 == hashM2);
+
+
+        int[] myArr = {3,6,8,9};
+        Set<Set<Integer>> result = generateSubsetsOfLengthK(myArr, 2);
+        System.out.println(Arrays.toString(result.toArray()));
+
+    }
+}
